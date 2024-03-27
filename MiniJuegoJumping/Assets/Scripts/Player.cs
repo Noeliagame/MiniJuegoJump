95fba43da788f9dc34b2ee952c9b3106c8d9f82f
Juan8516 <juan.ind85@gmail.com>
2024-03-27 13:49:28 -0500

    Script - Moviemiento
    Se crea funcionalidad para movimiento de personaje.

---
+++ MiniJuegoJumping/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float runSpeed = 1.0f;
+    public float jumSpeed = 3.0f;
+    private Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+           
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            _rigidbody.velocity = new Vector2(runSpeed,_rigidbody.velocity.y);
+        }
+        else if(Input.GetKey("a") || Input.GetKey("left"))
+        {
+            _rigidbody.velocity = new Vector2(-runSpeed, _rigidbody.velocity.y);
+        }
+        else
+        {
+            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        }
+    }
+}
